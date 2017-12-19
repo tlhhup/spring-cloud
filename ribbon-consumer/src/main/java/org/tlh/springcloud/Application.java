3681f8f09c08f8ab751a6bf81813d1d7ca861b2e
tlh <137273278@qq.com>
2017-12-19 22:21:44 +0800

    添加eureka应用

---
+++ ribbon-consumer/src/main/java/org/tlh/springcloud/Application.java
@@ -0,0 +1,25 @@
+package org.tlh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    @Bean
+    @LoadBalanced//开启客户端的负载均衡
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+
+}
