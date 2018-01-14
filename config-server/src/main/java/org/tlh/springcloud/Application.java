62b9f7b8df0af02bca42015f37a3356cbcff08c2
tlh <137273278@qq.com>
2018-01-14 10:59:56 +0800

    添加配置中心

---
+++ config-server/src/main/java/org/tlh/springcloud/Application.java
@@ -0,0 +1,16 @@
+package org.tlh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+
+}
